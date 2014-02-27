b5f377e5dfd635fb641d38854340cd4c5d530d27
Erik Vold <evold@mozilla.com>
2014-02-27 06:11:15 -0800

    tidying up, using arrow functions, making fewer emits, waiting for onload event before collecting metadata

--- data/cards-pagemod.js
+++ data/cards-pagemod.js
@@ -2,15 +2,14 @@
 setTimeout(function () {
 
   var metas = {};
-  var selector;
-  var nodes;
 
-  selector = "meta[property^=twitter],meta[name^=twitter],meta[property^=og],meta[name^=og]";
-  nodes = document.querySelectorAll(selector);
-  for (var i = 0; i < nodes.length; i++) {
-    ["name", "property"].forEach(function (key) {
+  let selector = "meta[property^=twitter],meta[name^=twitter],meta[property^=og],meta[name^=og]";
+  let nodes = document.querySelectorAll(selector);
+
+  for (let i = 0, l = nodes.length; i < l; i++) {
+    ["name", "property"].forEach(key => {
       if (nodes[i].hasAttribute(key)) {
-        ["value", "content"].forEach(function (value) {
+        ["value", "content"].forEach(value => {
           if (nodes[i].hasAttribute(value)) {
             metas[nodes[i].getAttribute(key)] = nodes[i].getAttribute(value);
           }
@@ -20,7 +19,10 @@ setTimeout(function () {
   }
 
   // <link rel="canonical" href="http://www.cbc.ca/news/world/vladimir-putin-s-ukraine-dilemma-how-to-react-1.2549953" />
-  // <link rel="image_src" href="http://i.cbc.ca/1.2550391.1393332876!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_460/russia.jpg" /> 
-  self.port.emit("metas", metas);
+  // <link rel="image_src" href="http://i.cbc.ca/1.2550391.1393332876!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_460/russia.jpg" />
+  if (Object.keys(metas).length) {
+    metas['url'] = document.location.href;
+    self.port.emit("metas", metas);
+  }
 
-}, 1000);
+}, 200);
