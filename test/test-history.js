be95731bf823efb2902946a8d28786f03d30b43d
Erik Vold <evold@mozilla.com>
2014-02-27 07:13:16 -0800

    Adding test for history and service/history modules

---
+++ test/test-history.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const tabs = require('sdk/tabs');
+const { on, once } = require('sdk/event/core');
+const { setTimeout } = require('sdk/timers');
+
+const { history } = require('about-history/history');
+const { History } = require('about-history/service/history');
+
+const { serve, host } = require('./httpd');
+
+function makeURL(title) encodeURI('data:text/html;charset=utf-8,' + title)
+
+exports["test history"] = function(assert, done) {
+  let title = 'testHistory1';
+  let url = host + title + '.html';
+  let srv = serve({ name: title });
+  let tab;
+
+  History.once('visit', data => {
+    assert.pass('visit event happened');
+    setTimeout(_ => {
+      history().then(results => {
+        assert.ok(results.length, 'there are results')
+        assert.ok(results.find(result => result.url == url), url + ' was found');
+        srv.stop(_ => tab.close(done));
+      })
+    })
+  });
+
+  tabs.open({
+    url: url,
+    onOpen: t => tab = t
+  })
+}
+
+require("sdk/test").run(exports);
