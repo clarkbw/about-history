65b62666fc6bf42bcd1b8bdf5ef420f8b5432493
Erik Vold <evold@mozilla.com>
2014-02-28 13:20:34 -0800

    bug fix for metadata and destroying the data collection page mod instances when they are done

--- data/cards-pagemod.js
+++ data/cards-pagemod.js
@@ -24,9 +24,7 @@ setTimeout(function () {
 
   // <link rel="canonical" href="http://www.cbc.ca/news/world/vladimir-putin-s-ukraine-dilemma-how-to-react-1.2549953" />
   // <link rel="image_src" href="http://i.cbc.ca/1.2550391.1393332876!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_460/russia.jpg" />
-  if (Object.keys(metas).length) {
-    metas['url'] = document.location.href;
-    self.port.emit("metas", metas);
-  }
+  metas['url'] = document.location.href;
+  self.port.emit("metas", metas);
 
 }, 200);
