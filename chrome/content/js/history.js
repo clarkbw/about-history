679180a6b7f7abd30479a03ed8631e370186ad35
Erik Vold <evold@mozilla.com>
2014-03-13 00:33:53 -0700

    removing a hack which is no longer necessary due to 739c41e2

--- chrome/content/js/history.js
+++ chrome/content/js/history.js
@@ -370,9 +370,3 @@ var Application = Backbone.View.extend({
 });
 
 var HistoryApp = new Application({el : $("#history-items")});
-
-// HACK: fixing #top links
-Array.slice(document.querySelectorAll("a[href^='#']")).forEach(link => {
-  let href = window.document.location.href.replace(/#.*/, '') + link.getAttribute('href');
-  link.setAttribute("href", href);
-});
